917b8c6ea72594cbad630fff94346866bd23e119
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-06-03 14:57:52 +0530

    Vitepress deploy 6

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,8 +9,8 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Home', link: '/' },  
+      { text: 'Advanced CSS', link: '/Udemy/S8_Advanced_CSS.md' },
     ],
 
     sidebar: [
@@ -31,6 +31,6 @@ export default defineConfig({
     ]
   },
   sitemap: {
-		hostname: 'https://vinitkesh.github.io',
+		hostname: 'https://vinitkesh.github.io/webdev.notes/',
 	},
 })
